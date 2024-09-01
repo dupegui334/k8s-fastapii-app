6794f46aaf09e5e292032ea675e99b0e7a4b913b
David Upegui <davidupegui334@hotmail.com>
2024-09-01 15:17:20 -0500

    containerize the app

--- app/main.py
+++ app/main.py
@@ -1,10 +1,10 @@
 from typing import Union
-
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"Hello": f"World from : {os.environ.get('ENV', 'DEFAULT')}"}
